d994de1d238380fefb737c2d264606f801d2e3e4
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-29 11:17:40 +0100

    File for installing relevant R packages.

---
+++ install.R
@@ -0,0 +1,11 @@
+## installations...
+
+require(remotes)
+remotes::install_github("sje30/sjedrp")
+remotes::install_github("sje30/sjedmin")
+remotes::install_github("sje30/sjevor")
+remotes::install_github("sje30/sjedist")
+
+## regular CRAN packages
+install.packages("spatstat")
+
