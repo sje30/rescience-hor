bf26c9fc33fce762f94137ba8f06e6f7305f5c4a
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-30 10:41:16 +0100

    rmarkdown bug (been fixed, but not on CRAN yet)

--- install.R
+++ install.R
@@ -10,7 +10,9 @@ install_github("sje30/sjedist")
 install.packages("spatstat")
 
 ## for rendering the paper
-install.packages("rmarkdown")
+install_github('rstudio/rmarkdown')
+## note bug with https://github.com/rstudio/rmarkdown/issues/1649
+## hence why rmarkdown package is required.
 
 ## tinytex takes a few minutes to setup, but needed for rendering pdf
 install.packages('tinytex')
