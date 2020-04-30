b9857008d7314479c13cc17998902b59150cb115
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-30 07:27:45 +0100

    Need rmarkdown package, of course.

--- install.R
+++ install.R
@@ -6,6 +6,8 @@ install_github("sje30/sjedmin")
 install_github("sje30/sjevor")
 install_github("sje30/sjedist")
 
-## regular CRAN packages
+## regular CRAN packages for project
 install.packages("spatstat")
 
+## for rendering the paper
+install.packages("rmarkdown")
